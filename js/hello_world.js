67ff8a33de23f6e527b84d68c97aae2396a87195
ongc366 <ongc366@newschool.edu>
2016-09-09 14:54:02 +0000

    intermediate changes, may develop further

--- js/hello_world.js
+++ js/hello_world.js
@@ -1,18 +1,18 @@
 var is_clicked = false;
 
 var random_x, 
-    random_y
+    random_y;
 
 var random = function(max) {
     var x = Math.floor(Math.random()*max);
     return x;
-}
+};
 
 var random_coords = function() {
-    random_x = random(window.innerWidth - 304);
-    random_y = random(window.innerHeight - 154);
+    random_x = random(window.innerWidth - 350);
+    random_y = random(window.innerHeight - 300);
     console.log(random_x, random_y);
-}
+};
 
 var choose = function() {
     for (var i = 1; i < 10; i++) {
@@ -20,9 +20,9 @@ var choose = function() {
         document.getElementById("f" + i).style.top = random_y;
         document.getElementById("f" + i).style.left = random_x;
     }
-}
+};
 
-var random_move = setInterval( function() { choose(); }, 3000)
+setInterval( function() { choose(); }, 3000);
 
 setTimeout(function() {
     choose();
@@ -39,5 +39,4 @@ document.getElementsByTagName('button')[0].onclick = function() {
         document.getElementById('site-title').style.display = "block";
         is_clicked = false;
     }
-    console.log('clicked');
-}
+};
