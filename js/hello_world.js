310e45085dc92a3e529b5d5cd3af4c8927104836
ongc366 <ongc366@newschool.edu>
2016-09-09 13:37:03 +0000

    first upload, still need to debug and change phrases

---
+++ js/hello_world.js
@@ -0,0 +1,43 @@
+var is_clicked = false;
+
+var random_x, 
+    random_y
+
+var random = function(max) {
+    var x = Math.floor(Math.random()*max);
+    return x;
+}
+
+var random_coords = function() {
+    random_x = random(window.innerWidth - 304);
+    random_y = random(window.innerHeight - 154);
+    console.log(random_x, random_y);
+}
+
+var choose = function() {
+    for (var i = 1; i < 10; i++) {
+        random_coords();
+        document.getElementById("f" + i).style.top = random_y;
+        document.getElementById("f" + i).style.left = random_x;
+    }
+}
+
+var random_move = setInterval( function() { choose(); }, 3000)
+
+setTimeout(function() {
+    choose();
+    document.getElementById('container').style.opacity = 1;
+}, 3000);
+
+choose();
+
+document.getElementsByTagName('button')[0].onclick = function() {
+    if (is_clicked === false) {
+        document.getElementById('site-title').style.display = "none";
+        is_clicked = true;
+    } else {
+        document.getElementById('site-title').style.display = "block";
+        is_clicked = false;
+    }
+    console.log('clicked');
+}
