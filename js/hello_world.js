0a850b0765b13870b784814075f76732eb54e877
ongc366 <ongc366@newschool.edu>
2016-09-14 18:08:38 +0000

    fixed console error in hello world and iframe, js linkage problem

--- js/hello_world.js
+++ js/hello_world.js
@@ -1,4 +1,4 @@
-var is_clicked = false;
+var bg_switch = false;
 
 var random_x, 
     random_y;
@@ -8,35 +8,25 @@ var random = function(max) {
     return x;
 };
 
-var random_coords = function() {
-    random_x = random(window.innerWidth - 350);
-    random_y = random(window.innerHeight - 300);
-    console.log(random_x, random_y);
-};
-
 var choose = function() {
-    for (var i = 1; i < 10; i++) {
-        random_coords();
-        document.getElementById("f" + i).style.top = random_y;
-        document.getElementById("f" + i).style.left = random_x;
+    for (var i = 0; i < 9; i++) {
+        random_x = random(window.innerWidth) - 100;
+        random_y = random(window.innerHeight) - 100;
+        document.getElementsByTagName("iframe")[i].style.left = random_x + "px";
+        document.getElementsByTagName("iframe")[i].style.top = random_y + "px";
     }
 };
 
-// setInterval( function() { choose(); }, 3000);
+clearInterval(shuffle);
+var shuffle = setInterval(function() {
+    choose();
+    if (bg_switch === false) {
+        document.body.style.backgroundColor = "black";
+        bg_switch = true;
+    } else {
+        document.body.style.backgroundColor = "white";
+        bg_switch = false;
+    }
+}, 2000)
 
-// setTimeout(function() {
-//     choose();
-//     document.getElementById('container').style.opacity = 1;
-// }, 3000);
-
-// choose();
-
-// document.getElementsByTagName('button')[0].onclick = function() {
-//     if (is_clicked === false) {
-//         document.getElementById('site-title').style.display = "none";
-//         is_clicked = true;
-//     } else {
-//         document.getElementById('site-title').style.display = "block";
-//         is_clicked = false;
-//     }
-// };
+choose();
